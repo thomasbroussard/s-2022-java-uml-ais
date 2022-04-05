ac310f9846b269342d60943dc2565f52e05a9269
Présentation <thomas.broussard@epita.fr>
2022-04-05 10:07:48 +0200

    * first commit

--- bank-account-system/src/fr/epita/bankaccount/datamodel/Account.java
+++ bank-account-system/src/fr/epita/bankaccount/datamodel/Account.java
@@ -6,7 +6,19 @@ public class Account {
     String accountId;
     BigDecimal balance;
 
+    public String getAccountId() {
+        return accountId;
+    }
 
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
 
+    public BigDecimal getBalance() {
+        return balance;
+    }
 
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
 }
