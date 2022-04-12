964df632e4e3620c77468e2e427da62db197eb38
Présentation <thomas.broussard@epita.fr>
2022-04-12 11:03:58 +0200

    * file reading operations and csv deserialization

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,5 @@ G
 +bank-account-system/bank-account-system.iml,7\a\7a7cecb9aef32dc526997dbb60cad48762d12ef5
 D
 class-diagram.drawio,1\3\134bea9dc17c2289b9673add6c984290ebce6701
+z
+Jbank-account-system/src/fr/epita/bankaccount/datamodel/SavingsAccount.java,3\3\336d0e73b02c63399276ef1e512c1a606ac6c8c5
--- bank-account-system/src/fr/epita/bankaccount/launchers/Launcher.java
+++ bank-account-system/src/fr/epita/bankaccount/launchers/Launcher.java
@@ -2,9 +2,13 @@ package fr.epita.bankaccount.launchers;
 
 import fr.epita.bankaccount.datamodel.Customer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Launcher {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("hello!");
 
         Customer customer = new Customer("thomas", "paris");
@@ -12,6 +16,42 @@ public class Launcher {
         System.out.println(customer.getName());
         System.out.println(customer.getAddress());
 
+
+        File file = new File("./bank-account-system/customers.csv");
+
+        Scanner scanner = new Scanner(file);
+        scanner.nextLine();
+        List<Customer> customerList = new ArrayList<>();
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            String[] parts = line.split(",");
+            System.out.println("name:" + parts[0]);
+            System.out.println("address:" + parts[1]);
+            Customer currentCustomer = new Customer(parts[0], parts[1]);
+            currentCustomer.setAge(Double.parseDouble(parts[2]));
+            customerList.add(currentCustomer);
+        }
+
+
+        double[][] matrix = new double[customerList.size()][2];
+
+        Map<String,Integer> cityEncoding  = new LinkedHashMap<>();
+
+        for (int i = 0; i < customerList.size(); i ++){
+            Customer currentCustomer = customerList.get(i);
+            double[] customerVector = new double[2];
+            Integer encodedCityValue = cityEncoding.get(currentCustomer.getAddress());
+            if (encodedCityValue == null){
+                encodedCityValue = cityEncoding.size();
+                cityEncoding.put(currentCustomer.getAddress(), encodedCityValue);
+            }
+            customerVector[0] = encodedCityValue;
+            customerVector[1] = currentCustomer.getAge();
+            matrix[i] = customerVector;
+        }
+
+        System.out.println(cityEncoding);
+        System.out.println(Arrays.deepToString(matrix));
 
     }
 }
