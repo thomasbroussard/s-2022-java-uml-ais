667cdd2f7ac33a75b90e2ecee968489eccf6709b
Présentation <thomas.broussard@epita.fr>
2022-05-09 12:11:44 +0200

    * titanic module creation

--- titanic-passengers/src/fr/epita/exercises/titanic/datamodel/Passenger.java
+++ titanic-passengers/src/fr/epita/exercises/titanic/datamodel/Passenger.java
@@ -2,5 +2,67 @@ package fr.epita.exercises.titanic.datamodel;
 
 public class Passenger {
 
+    private int pclass;
+    private int survived;
+    private int passengerId;
+    private String sex;
+    private int age;
+    private String embarked;
 
+    public Passenger(int pclass, int survived, int passengerId, String sex, int age, String embarked) {
+        this.pclass = pclass;
+        this.survived = survived;
+        this.passengerId = passengerId;
+        this.sex = sex;
+        this.age = age;
+        this.embarked = embarked;
+    }
+
+    public int getPclass() {
+        return pclass;
+    }
+
+    public void setPclass(int pclass) {
+        this.pclass = pclass;
+    }
+
+    public int getSurvived() {
+        return survived;
+    }
+
+    public void setSurvived(int survived) {
+        this.survived = survived;
+    }
+
+    public int getPassengerId() {
+        return passengerId;
+    }
+
+    public void setPassengerId(int passengerId) {
+        this.passengerId = passengerId;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmbarked() {
+        return embarked;
+    }
+
+    public void setEmbarked(String embarked) {
+        this.embarked = embarked;
+    }
 }
--- titanic-passengers/src/fr/epita/exercises/titanic/launcher/Launcher.java
+++ titanic-passengers/src/fr/epita/exercises/titanic/launcher/Launcher.java
@@ -1,10 +1,11 @@
 package fr.epita.exercises.titanic.launcher;
 
+import fr.epita.exercises.titanic.datamodel.Passenger;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Launcher {
 
@@ -16,10 +17,54 @@ public class Launcher {
 
         stringList.remove(0);
         System.out.println(stringList);
+        List<Passenger> passengers = new ArrayList<>();
+
+        List<String> faultyPassengerIds = new ArrayList<>();
         for (String line : stringList){
             String[] parts = line.split(",");
             System.out.println("pclass: " + parts[2]);
+
+            try {
+                Passenger p = new Passenger(Integer.parseInt(parts[2])
+                        , Integer.parseInt(parts[1])
+                        , Integer.parseInt(parts[0])
+                        , parts[3]
+                        , Integer.parseInt(parts[4])
+                        , parts[10]);
+                passengers.add(p);
+            }catch (Exception e){
+                e.printStackTrace();
+                faultyPassengerIds.add(parts[0]);
+            }
         }
+        System.out.println(passengers);
+        System.out.println("faulty passengers size:" + faultyPassengerIds.size());
+
+        Map<String, Integer> sexEncoding = new LinkedHashMap<>();
+        sexEncoding.put("male", 0);
+        sexEncoding.put("female",1);
+
+        Map<String, Integer> embarkedEncoding = new LinkedHashMap<>();
+        embarkedEncoding.put("S", 0);
+        embarkedEncoding.put("Q",1);
+        embarkedEncoding.put("C",2);
+
+        double[][] passengerMatrix = new double[passengers.size()][];
+        int i = 0;
+        for (Passenger passenger : passengers){
+            double[] passengerAsVector = new double[6];
+            passengerAsVector[0] = passenger.getPassengerId();
+            passengerAsVector[1] = passenger.getSurvived();
+            passengerAsVector[2] = passenger.getPclass();
+            passengerAsVector[3] = sexEncoding.get(passenger.getSex());
+            passengerAsVector[4] = passenger.getAge();
+            passengerAsVector[5] = embarkedEncoding.get(passenger.getEmbarked());
+            passengerMatrix[i] = passengerAsVector;
+            i++;
+        }
+        System.out.println(Arrays.deepToString(passengerMatrix));
+
+
     }
 
 
